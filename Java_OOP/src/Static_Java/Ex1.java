6e0a9f35af76bae742fc13073380aa5bbd2f61a1
Lâm Đức Kính <89448648+KinhLam@users.noreply.github.com>
2024-08-03 23:52:34 +0700

    Update code Ex_2

--- Java_OOP/src/Static_Java/Ex1.java
+++ Java_OOP/src/Static_Java/Ex1.java
@@ -1,5 +1,19 @@
 package Static_Java;
 
 public class Ex1 {
+    public static int sumOfArray(int arr[]) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
 
+    public static double sumOfArray(double arr[]) {
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
 }
--- Java_OOP/src/Static_Java/Ex2.java
+++ Java_OOP/src/Static_Java/Ex2.java
@@ -1,5 +1,28 @@
 package Static_Java;
 
+class Student {
+    private String name;
+    private int age;
+
+    public static int numberOfStudents;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+        numberOfStudents++;
+    }
+}
+
+class Entry {
+    public static void main(String[] args) {
+        Student s1 = new Student("Manh", 19);
+        Student s2 = new Student("Trung", 19);
+        Student s3 = new Student("Kien", 19);
+
+        System.out.println(Student.numberOfStudents);
+    }
+}
+
 public class Ex2 {
 
 }
